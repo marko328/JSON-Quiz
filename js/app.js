c646d2ef2f9d1ed887c374cbb116a0983e600237
Marko X <74780980+marko328@users.noreply.github.com>
2021-07-19 15:49:19 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,105 @@
+const url = "./data/quiz.json";
+const app = document.getElementById("app");
+
+(async () => {
+    try {
+        let promise = await fetch(url);
+        if (!promise.ok) {
+            throw Error("Error while reading file");
+        }
+        let quiz = await promise.json();
+        let {
+            q1,
+            q2,
+            q3,
+            q4
+        } = quiz.quiz;
+
+        app.innerHTML += `<div class="box"><h2>1. ${q1.question}</h2>
+      <ul>${q1.options
+        .map((item) => {
+          return `<li><label><input id="input" type="radio" value="${item}" name="q1"> ${item}</label></li>`;
+        })
+        .join("")}</ul></div>
+        <div class="box"><h2>2. ${q2.question}</h2>
+      <ul>${q2.options
+        .map((item) => {
+          return `<li><label><input id="input" type="radio" value="${item}" name="q2"> ${item}</label></li>`;
+        })
+        .join("")}</ul></div>
+        <div class="box"><h2>3. ${q3.question}</h2>
+      <ul>${q3.options
+        .map((item) => {
+          return `<li><label><input id="input" type="radio" value="${item}" name="q3"> ${item}</label></li>`;
+        })
+        .join("")}</ul></div>
+        <div class="box"><h2>4. ${q4.question}</h2>
+      <ul>${q4.options
+        .map((item) => {
+          return `<li><label><input id="input" type="radio" value="${item}" name="q4"> ${item}</label></li>`;
+        })
+        .join("")}</ul></div>
+    `;
+
+
+        (() => {
+            let radios = document.getElementsByName("q1");
+            let val = localStorage.getItem('q1');
+            for (let i = 0; i < radios.length; i++) {
+                if (radios[i].value == val) {
+                    radios[i].checked = true;
+                }
+            }
+            document.getElementsByName('q1').forEach(e => {
+                e.onchange = () => {
+                    localStorage.setItem('q1', e.value);
+                }
+            })
+        })();
+        (() => {
+            let radios = document.getElementsByName("q2");
+            let val = localStorage.getItem('q2');
+            for (let i = 0; i < radios.length; i++) {
+                if (radios[i].value == val) {
+                    radios[i].checked = true;
+                }
+            }
+            document.getElementsByName('q2').forEach(e => {
+                e.onchange = () => {
+                    localStorage.setItem('q2', e.value);
+                }
+            })
+        })();
+        (() => {
+            let radios = document.getElementsByName("q3");
+            let val = localStorage.getItem('q3');
+            for (let i = 0; i < radios.length; i++) {
+                if (radios[i].value == val) {
+                    radios[i].checked = true;
+                }
+            }
+            document.getElementsByName('q3').forEach(e => {
+                e.onchange = () => {
+                    localStorage.setItem('q3', e.value);
+                }
+            })
+        })();
+        (() => {
+            let radios = document.getElementsByName("q4");
+            let val = localStorage.getItem('q4');
+            for (let i = 0; i < radios.length; i++) {
+                if (radios[i].value == val) {
+                    radios[i].checked = true;
+                }
+            }
+            document.getElementsByName('q4').forEach(e => {
+                e.onchange = () => {
+                    localStorage.setItem('q4', e.value);
+                }
+            })
+        })();
+
+    } catch (err) {
+        console.log(err);
+    }
+})();
